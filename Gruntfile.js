a21243ad1b4ffb9de24c1197ddeb15b09e4d5c0e
brotherilia <ilyamenshikov@gmail.com>
2016-09-11 04:01:28 +0300

    2 цветовые схемы

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,13 +61,99 @@ module.exports = function(grunt) {
         files: {
           "build/index.html": ["build/index.html"],
           "build/about.html": ["build/about.html"],
+
+          "build/diary_3.html": ["build/diary_3.html"],
+
           "build/overview.html": ["build/overview.html"],
           "build/service.html": ["build/service.html"],
+
+          "build/files.html": ["build/files.html"],
+          "build/links.html": ["build/links.html"],
+
+          "build/manual_cont.html": ["build/manual_cont.html"],
+          "build/mainten_sched.html": ["build/mainten_sched.html"],
+          "build/parts_compare.html": ["build/parts_compare.html"],
+          "build/steed_modif.html": ["build/steed_modif.html"],
+
+          "build/gallery.html": ["build/gallery.html"],
+          "build/gallery1.html": ["build/gallery1.html"],
+          "build/gallery2.html": ["build/gallery2.html"],
+          "build/gallery3.html": ["build/gallery3.html"],
+          "build/gallery4.html": ["build/gallery4.html"],
+          "build/gallery6.html": ["build/gallery6.html"],
+          "build/gallery7.html": ["build/gallery7.html"],
+          "build/gallery8.html": ["build/gallery8.html"],
+          "build/gallery9.html": ["build/gallery9.html"],
+          "build/gallery10.html": ["build/gallery10.html"],
+          "build/gallery11.html": ["build/gallery11.html"],
+          "build/gallery12.html": ["build/gallery12.html"],
+          "build/gallery13.html": ["build/gallery13.html"],
+          "build/gallery14.html": ["build/gallery14.html"],
+          "build/gallery15.html": ["build/gallery15.html"],
+          "build/gallery17.html": ["build/gallery17.html"],
+          "build/gallery20.html": ["build/gallery20.html"],
+          "build/gallery22.html": ["build/gallery22.html"],
+
+          "build/steedbreed.html": ["build/steedbreed.html"],
+          "build/sb_accessoires.html": ["build/sb_accessoires.html"],
+          "build/sb_append_1.html": ["build/sb_append_1.html"],
+          "build/sb_append_2.html": ["build/sb_append_2.html"],
+          "build/sb_append_3.html": ["build/sb_append_3.html"],
+          "build/sb_charging.html": ["build/sb_charging.html"],
+          "build/sb_cooling.html": ["build/sb_cooling.html"],
+          "build/sb_frame_etc.html": ["build/sb_frame_etc.html"],
+          "build/sb_frontwheel.html": ["build/sb_frontwheel.html"],
+          "build/sb_fuelsystem.html": ["build/sb_fuelsystem.html"],
+          "build/sb_hydrabrake.html": ["build/sb_hydrabrake.html"],
+          "build/sb_ignition.html": ["build/sb_ignition.html"],
+          "build/sb_lights_switches.html": ["build/sb_lights_switches.html"],
+          "build/sb_lubricat.html": ["build/sb_lubricat.html"],
+          "build/sb_maintenance.html": ["build/sb_maintenance.html"],
+          "build/sb_meters.html": ["build/sb_meters.html"],
+          "build/sb_steering.html": ["build/sb_steering.html"],
+          "build/sb_transmission.html": ["build/sb_transmission.html"],
+
           "build/articles_rev.html": ["build/articles_rev.html"],
           "build/articles_serv.html": ["build/articles_serv.html"],
           "build/articles_beg.html": ["build/articles_beg.html"],
-          "build/files.html": ["build/files.html"],
-          "build/links.html": ["build/links.html"]
+
+          "build/articles_beg_1.html": ["build/articles_beg_1.html"],
+          "build/articles_beg_2.html": ["build/articles_beg_2.html"],
+          "build/articles_beg_3.html": ["build/articles_beg_3.html"],
+
+          "build/articles_serv_0.html": ["build/articles_serv_0.html"], //FIXME
+          "build/articles_serv_1.html": ["build/articles_serv_1.html"], //FIXME
+          "build/articles_serv_2.html": ["build/articles_serv_2.html"], //FIXME
+          "build/articles_serv_3.html": ["build/articles_serv_3.html"], //FIXME
+          "build/articles_serv_4.html": ["build/articles_serv_4.html"], //FIXME
+          "build/articles_serv_5.html": ["build/articles_serv_5.html"], //FIXME
+          "build/articles_serv_6.html": ["build/articles_serv_6.html"], //FIXME
+          "build/articles_serv_7.html": ["build/articles_serv_7.html"], //FIXME
+          "build/articles_serv_8.html": ["build/articles_serv_8.html"], //FIXME
+          "build/articles_serv_9.html": ["build/articles_serv_9.html"], //FIXME
+          "build/articles_serv_10.html": ["build/articles_serv_10.html"], //FIXME
+          "build/articles_serv_11.html": ["build/articles_serv_11.html"], //FIXME
+          "build/articles_serv_12.html": ["build/articles_serv_12.html"], //FIXME
+          "build/articles_serv_13.html": ["build/articles_serv_13.html"], //FIXME
+          "build/articles_serv_14.html": ["build/articles_serv_14.html"], //FIXME
+          "build/articles_serv_15.html": ["build/articles_serv_15.html"], //FIXME
+          "build/articles_serv_16.html": ["build/articles_serv_16.html"], //FIXME
+          "build/articles_serv_17.html": ["build/articles_serv_17.html"], //FIXME
+          "build/articles_serv_18.html": ["build/articles_serv_18.html"], //FIXME
+          "build/articles_serv_19.html": ["build/articles_serv_19.html"], //FIXME
+          "build/articles_serv_20.html": ["build/articles_serv_20.html"], //FIXME
+          "build/articles_serv_21.html": ["build/articles_serv_21.html"], //FIXME
+
+          "build/articles_rev_1.html": ["build/articles_rev_1.html"],
+          "build/articles_rev_2.html": ["build/articles_rev_2.html"],
+          "build/articles_rev_3.html": ["build/articles_rev_3.html"],
+          "build/articles_rev_4.html": ["build/articles_rev_4.html"],
+          "build/articles_rev_5.html": ["build/articles_rev_5.html"],
+          "build/articles_rev_6.html": ["build/articles_rev_6.html"],
+          "build/articles_rev_7.html": ["build/articles_rev_7.html"],
+          "build/articles_rev_8.html": ["build/articles_rev_8.html"],
+          "build/articles_rev_9.html": ["build/articles_rev_9.html"],
+          "build/articles_rev_10.html": ["build/articles_rev_10.html"]
         }
       }
     },
@@ -76,7 +162,12 @@ module.exports = function(grunt) {
     less: {
       style: {
         files: {
-          "build/css/style.css": "src/less/style.less"
+          "build/css/style--dark.css": "src/less/style--dark.less"
+        }
+      },
+      stylelight: {
+        files: {
+          "build/css/style--light.css": "src/less/style--light.less"
         }
       }
     },
@@ -95,7 +186,10 @@ module.exports = function(grunt) {
         ]
       },
       style: {
-        src: "build/css/style.css"
+        src: "build/css/style--dark.css"
+      },
+      stylelight: {
+        src: "build/css/style--light.css"
       }
     },
 
@@ -106,7 +200,8 @@ module.exports = function(grunt) {
           config: "csscomb.json"
         },
         files: {
-          "build/css/style.css": ["build/css/style.css"]
+          "build/css/style--dark.css": ["build/css/style--dark.css"],
+          "build/css/style--light.css": ["build/css/style--light.css"]
         }
       }
     },
@@ -114,8 +209,15 @@ module.exports = function(grunt) {
     //*** Конкатенация CSS ***//
     concat: {
       css: {
-        src: [ "build/css/normalize.css", "build/css/social-likes.css", "build/css/style.css" ],
-        dest: "build/css/style.css",
+        src: [ "build/css/normalize.css", "build/css/social-likes.css", "build/css/style--dark.css" ],
+        dest: "build/css/style--dark.css",
+        options: {
+          separator: "\n\r/***** CONCATENATION HERE! *****/\n\r"
+        }
+      },
+      csslight: {
+        src: [ "build/css/normalize.css", "build/css/social-likes.css", "build/css/style--light.css" ],
+        dest: "build/css/style--light.css",
         options: {
           separator: "\n\r/***** CONCATENATION HERE! *****/\n\r"
         }
@@ -129,7 +231,8 @@ module.exports = function(grunt) {
           report: "gzip"
         },
         files: {
-          "build/css/style.min.css": ["build/css/style.css"]
+          "build/css/style--dark.min.css": ["build/css/style--dark.css"],
+          "build/css/style--light.min.css": ["build/css/style--light.css"]
         }
       }
     },
@@ -172,7 +275,7 @@ module.exports = function(grunt) {
     //*** Отслеживание изменений в исходниках ***//
     watch: {
       html: {
-        files: ["src/**/*.html","src/tmp/ugc/*.html"],
+        files: ["src/**/*.html"],
         tasks: ["copy:html","processhtml"],
         options: {spawn: false}
       },
@@ -205,7 +308,7 @@ module.exports = function(grunt) {
     secret: grunt.file.readJSON("authssh.json"),
     sshexec: {
       clean: {
-        command: ["cd test.menshikov.su/www; rm -rf css img js fonts; rm -f *.html favicon.ico"],
+        command: ["cd hondasteed.org.ru/www; rm -rf css img js fonts; rm -f *.html favicon.ico"],
         options: {
           host: "<%= secret.host %>",
           username: "<%= secret.username %>",
@@ -226,7 +329,7 @@ module.exports = function(grunt) {
             cwd: "build/",
             src: ["**/*", "!tmp/**/*"],
             filter: "isFile",
-            dest: "test.menshikov.su/www"
+            dest: "hondasteed.org.ru/www"
         }]
       }
     }
--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
       $("#popup").hide();
       $("#menu").css({"display":"block"});
       $("#tools").css({"display":"none"});
-      $("#scroll").css({"transform":"rotate(225deg)"}).css({"top":"0"});
+      $("#scroll").css({"transform":"rotate(225deg)"}).css({"top":"-5px"});
     }
   });
 
@@ -38,24 +38,59 @@ $(document).ready(function(){
     var displayStatus  = $("#menu").css("display");
     event.preventDefault();
     if (displayStatus == "block"){
-      $(this).css({"transform":"rotate(45deg)"}).css({"top":"20px"});
+      $(this).css({"transform":"rotate(45deg)"}).css({"top":"15px"});
       $("#menu").hide();
       $("#tools").fadeIn();
     }
     else {
-      $(this).css({"transform":"rotate(225deg)"}).css({"top":"0"});
+      $(this).css({"transform":"rotate(225deg)"}).css({"top":"-5px"});
       $("#tools").hide();
       $("#menu").fadeIn();
     }
   });
 
+  // Всплывающее окно с изображением
+
+  $(".js-img-link").click(function(event){
+    event.preventDefault();
+    var popupBgr = $(this).attr("id");
+    var popupTitle = $(this).attr("title");
+    var popupAlt = $(this).attr("alt");
+    var popupName = $(this).attr("name");
+    $(".img-popup__main").css({"background-image": "url('../"+popupBgr+"')"});
+    if (popupTitle){
+      $(".img-popup__header").text(popupTitle).css({"display": "block"});
+    }
+    else {
+      $(".img-popup__header").css({"display": "none"});
+    }
+    if (popupAlt){
+      $(".img-popup__footer").text(popupAlt).css({"display": "block"});
+    }
+    else if (popupName){
+      $(".img-popup__footer").text(popupName).css({"display": "block"});
+    }
+    else {
+      $(".img-popup__footer").css({"display": "none"});
+    }
+    $(".img-popup").fadeIn().css({"display": "flex"});
+  });
+  $(".img-popup").click(function(event){
+    $(this).fadeOut();
+  });
+
   // Плавная прокрутка страницы наверх
 
 	$(window).scroll(function(){
+    var displayMode  = $("#cssSwitch").css("position");
+    if (displayMode == "fixed"){
+      $("#cssSwitch").css({"display": "none"});
+    }
 		if ($(this).scrollTop() > 100) {
 			$('#to-top').fadeIn();
 		}
 		else {
+      $("#cssSwitch").css({"display": "block"});
 			$('#to-top').hide();
 		}
 	});
@@ -63,4 +98,19 @@ $(document).ready(function(){
 		$("html, body").animate({ scrollTop: 0 }, 600);
 		return false;
 	});
+
+// Смена цветовой схемы
+
+  $("#cssSwitch").click(function(event){
+    var currentCSS = $("#stylesheet").attr("href");
+    event.preventDefault();
+    if (currentCSS == "css/style--dark.min.css"){
+      $("#stylesheet").attr("href", "css/style--light.min.css");
+      $("#cssSwitchIcon").attr("class", "css-icon css-icon--moon");
+    }
+    else {
+      $("#stylesheet").attr("href", "css/style--dark.min.css");
+      $("#cssSwitchIcon").attr("class", "css-icon css-icon--sun");
+    }
+  });
 });
